47c443f600ad7008409ca4f647cf221414f1bc67
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-12 15:44:57 +0300

    created SideDrawer component,  fixed importing  icons

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer'
+
 export default class Header extends Component {
+  state = {
+    drawerOpen: false
+  }
+
+  toggleDrawer = (value) => {
+    this.setState({
+      drawerOpen: value
+    });
+  } 
+
   render() {
     return (
       <AppBar
@@ -21,6 +33,17 @@ export default class Header extends Component {
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">The adventure begins</div>
           </div>
+          <IconButton 
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SideDrawer 
+            open={this.state.drawerOpen}
+            onClose={this.toggleDrawer}
+          />
         </ToolBar>
       </AppBar>
     )
