d35f893029b180223a169826a7e00b4148b66035
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-13 00:59:38 +0300

    added MyButton reusable component

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
+import MyButton from '../utils/MyButton';
 
 class Discount extends Component {
   state = {
@@ -39,7 +40,12 @@ class Discount extends Component {
             <div className="discount_description">
               <h3>Get ready for new adventures and purchase tickets before 20th JUNE</h3>
               <p>Nisi non consequat non incididunt reprehenderit. Laboris velit nulla minim in anim aliquip mollit ex amet enim ipsum anim. Reprehenderit sunt ex enim cillum aliqua consectetur ipsum cillum.</p>
-              <div></div>
+              <MyButton 
+                text="Purchase Tickets"
+                bck="#ffa800"
+                color="#fff"
+                link="https://github.com/kirillzhurin"
+              />
             </div>
           </Slide>
         </div>
